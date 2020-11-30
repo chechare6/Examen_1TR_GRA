db649cdf2b3a2349778f376dbd27d13deea7df84
César García Viana <cesargarciaviana@gmail.com>
2020-11-30 16:48:05 +0100

    Movimiento camara y movimiento jugador
    He hecho que la cámara siga al jugador a cierta distancia, no está suavizado. He reducido el Field of view de la cámara para que se vea mejor
    He hecho que el jugador se mueva con los joysticks, tiene el movimiento limitado.
    
    El anterior commit nome ha funcionado el gittignore, por eso vuelvo a comentar lo del movimiento del jugador

---
+++ GarciaViana_Cesar/Assets/Scripts/MovCamara.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovCamara : MonoBehaviour
+{
+    [SerializeField] Transform spherePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(spherePos.position.x, spherePos.position.y +6, spherePos.position.z -3);
+    }
+}
--- GarciaViana_Cesar/Assets/Scripts/Sphere.cs
+++ GarciaViana_Cesar/Assets/Scripts/Sphere.cs
@@ -8,7 +8,7 @@ public class Sphere : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = 5f;
+        speed = 15f;
     }
 
     // Update is called once per frame
