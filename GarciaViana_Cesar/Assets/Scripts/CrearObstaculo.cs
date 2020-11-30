c12e83652641e97399fe5da044786621b082d6a7
César García Viana <cesargarciaviana@gmail.com>
2020-11-30 17:13:51 +0100

    Creada corrutina y metodo que genera obstaculos
    He creado un metodo que genera los obstaculos y una corrutina que los genera automaticamente al comenzar el juego. (falta que se destruyan y ajustar tiempos).

---
+++ GarciaViana_Cesar/Assets/Scripts/CrearObstaculo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrearObstaculo : MonoBehaviour
+{
+    [SerializeField] GameObject Obstaculo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ObstaculoCoroutine");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GenerarObstaculo();
+        }
+    }
+
+   void GenerarObstaculo(){
+       float randomX = Random.Range(-9.5f,9.5f);
+       float randomZ = Random.Range(-9.5f,9.5f);
+
+       Vector3 RndmPos = new Vector3(randomX,0,randomZ);
+       Instantiate(Obstaculo, RndmPos, Quaternion.identity);
+
+    }
+
+    IEnumerator ObstaculoCoroutine()
+    {
+        for(int n=0; ; n++){
+            
+            GenerarObstaculo();
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
